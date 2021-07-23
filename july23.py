720fb14d7291eae7a26dd5bffa0a7ebda9c196f5
Jon Mockbee <mockbee1000@gmail.com>
2021-07-23 16:52:10 -0400

    else if elif limited commits did it all at once

--- july23.py
+++ july23.py
@@ -1,4 +1,12 @@
 year = int(input("Enter a year: "))
-if 
-elif 
-else 
+if  year % 4!= 0:
+    print ("common")
+
+elif year % 100!= 0:
+    print ("leap year")
+
+elif year % 400!= 0:
+    print ("common")
+else:
+    print('leap')
+print("The year is :", year)
